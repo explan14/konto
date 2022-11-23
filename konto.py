0ba798814c21db7b54a6ffcd12152a1af20b4008
explan14 <118926568+explan14@users.noreply.github.com>
2022-11-23 19:26:12 +0100

    Update konto.py

--- konto.py
+++ konto.py
@@ -1,4 +1,8 @@
-"das ist mein main klasse was ich imoprtet habe ist os für die files zu lesen , dann hab ich mein konto_module importiert
+"das ist mein main klasse was ich imoprtet habe ist os für die files zu lesen , dann hab ich mein konto_module importiert im konto modul sind die funktionen wie passwort eingeben die karte eingeben und überprüfen."
+"und keyboard libary um gewisse tasten auf mein tastatur zu benutzen . Gewissen tasten haben nicht auf mein laptop funktioniert darum die libary. dann time für denn zeit stoppen wo ich meine optionen aus wähle"
+" dann hab ich crypto.py module ein webscrapper gebaut mit pandas , beautifullsoap und request "
+" das project hab ich nicht abgeschlossen weil zu viel ideen gehabt das ich keine überblieg gehabt habe "
+
 
 
 
